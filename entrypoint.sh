8dd73a0bd1649e8b94f4951329429768eddfc34b
nalbam <me@nalbam.com>
2019-09-21 04:55:19 +0900

    add _docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -269,6 +269,57 @@ END
   fi
 }
 
+_docker_pre() {
+  if [ -z "${USERNAME}" ]; then
+    _error "USERNAME is not set."
+  fi
+
+  if [ -z "${PASSWORD}" ]; then
+    _error "PASSWORD is not set."
+  fi
+
+  if [ -z "${IMAGE_NAME}" ]; then
+    _error "IMAGE_NAME is not set."
+  fi
+
+  if [ -z "${TAG_NAME}" ]; then
+    if [ -f ./target/TAG_NAME ]; then
+      TAG_NAME=$(cat ./target/TAG_NAME | xargs)
+    elif [ -f ./target/VERSION ]; then
+      TAG_NAME=$(cat ./target/VERSION | xargs)
+    elif [ -f ./VERSION ]; then
+      TAG_NAME=$(cat ./VERSION | xargs)
+    fi
+    if [ -z "${TAG_NAME}" ]; then
+      _error "TAG_NAME is not set."
+    fi
+  fi
+}
+
+_docker() {
+  _docker_pre
+
+  echo "docker login -u ${USERNAME}"
+  echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin
+
+  echo "docker build -t ${IMAGE_NAME}:${TAG_NAME} ."
+  docker build -t ${IMAGE_NAME}:${TAG_NAME} .
+
+  echo "docker push ${IMAGE_NAME}:${TAG_NAME}"
+  docker push ${IMAGE_NAME}:${TAG_NAME}
+
+  if [ ! -z "${LATEST}" ]; then
+    echo "docker tag ${IMAGE_NAME}:latest"
+    docker tag ${IMAGE_NAME}:${TAG_NAME} ${IMAGE_NAME}:latest
+
+    echo "docker push ${IMAGE_NAME}:latest"
+    docker push ${IMAGE_NAME}:latest
+  fi
+
+  echo "docker logout"
+  docker logout
+}
+
 _slack_pre() {
   if [ -z "${SLACK_TOKEN}" ]; then
     _error "SLACK_TOKEN is not set."
@@ -303,6 +354,9 @@ case "${CMD}" in
     ;;
   --release|release)
     _release
+    ;;
+  --docker|docker)
+    _docker
     ;;
   --slack|slack)
     _slack
