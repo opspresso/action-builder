6f59faf68e452a7fa7b1324709d8f4fa5941aaa8
nalbam <me@nalbam.com>
2021-03-15 17:04:24 +0900

    v0.3.11

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,35 @@ REPOSITORY=${GITHUB_REPOSITORY}
 USERNAME=${USERNAME:-$GITHUB_ACTOR}
 REPONAME=$(echo "${REPOSITORY}" | cut -d'/' -f2)
 
-_error() {
-  echo -e "$1"
+command -v tput > /dev/null && TPUT=true
 
+_echo() {
+  if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
+    echo -e "$(tput setaf $2)$1$(tput sgr0)"
+  else
+    echo -e "$1"
+  fi
+}
+
+_result() {
+  echo
+  _echo "# $@" 4
+}
+
+_command() {
+  echo
+  _echo "$ $@" 3
+}
+
+_success() {
+  echo
+  _echo "+ $@" 2
+  exit 0
+}
+
+_error() {
+  echo
+  _echo "- $@" 1
   if [ "${LOOSE_ERROR}" == "true" ]; then
     exit 0
   else
@@ -44,7 +70,7 @@ _version() {
     printf "v0.0.x" > ./VERSION
   fi
 
-  echo "GITHUB_REF: ${GITHUB_REF}"
+  _result "GITHUB_REF: ${GITHUB_REF}"
 
   # release version
   MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
@@ -76,7 +102,7 @@ _version() {
       VERSION="${MAJOR}.${MINOR}.0"
     fi
 
-    echo "VERSION: ${VERSION}"
+    _result "VERSION: ${VERSION}"
 
     # new version
     if [ "${GITHUB_REF}" == "refs/heads/main" ] || [ "${GITHUB_REF}" == "refs/heads/master" ]; then
@@ -100,7 +126,7 @@ _version() {
     fi
   fi
 
-  echo "VERSION: ${VERSION}"
+  _result "VERSION: ${VERSION}"
 }
 
 _commit_pre() {
@@ -133,21 +159,21 @@ _commit() {
   git config --global user.name "${GIT_USERNAME}"
   git config --global user.email "${GIT_USEREMAIL}"
 
-  echo "git checkout ${GIT_BRANCH}"
+  _command "git checkout ${GIT_BRANCH}"
   git checkout ${GIT_BRANCH}
 
-  echo "git add --all"
+  _command "git add --all"
   git add --all
 
-  echo "git commit -m ${MESSAGE}"
+  _command "git commit -m ${MESSAGE}"
   git commit -a --allow-empty-message -m "${MESSAGE}"
 
   HEADER=$(echo -n "${GITHUB_ACTOR}:${GITHUB_TOKEN}" | base64)
 
-  echo "git push -u origin ${GIT_BRANCH}"
+  _command "git push -u origin ${GIT_BRANCH}"
   git -c http.extraheader="AUTHORIZATION: basic ${HEADER}" push -u origin ${GIT_BRANCH}
 
-  # echo "git push github.com/${GITHUB_REPOSITORY} ${GIT_BRANCH}"
+  # _command "git push github.com/${GITHUB_REPOSITORY} ${GIT_BRANCH}"
   # git push -q https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git ${GIT_BRANCH}
 }
 
@@ -180,7 +206,7 @@ text
 EOF
 
   # aws s3 sync
-  echo "aws s3 sync ${FROM_PATH} ${DEST_PATH}"
+  _command "aws s3 sync ${FROM_PATH} ${DEST_PATH}"
   aws s3 sync ${FROM_PATH} ${DEST_PATH} ${OPTIONS}
 
   _error_check
@@ -193,7 +219,7 @@ EOF
       # aws cf reset
       CFID=$(aws cloudfront list-distributions --query "DistributionList.Items[].{Id:Id,Origin:Origins.Items[0].DomainName}[?contains(Origin,'${BUCKET}')] | [0]" | awk '{print $1}')
       if [ "${CFID}" != "" ]; then
-          echo "aws cloudfront create-invalidation ${CFID}"
+          _command "aws cloudfront create-invalidation ${CFID}"
           aws cloudfront create-invalidation --distribution-id ${CFID} --paths "/*"
       fi
     fi
@@ -248,7 +274,7 @@ _release_id() {
 
   RELEASE_ID=$(cat /tmp/releases | TAG_NAME=${TAG_NAME} jq -r '.[] | select(.tag_name == env.TAG_NAME) | .id' | xargs)
 
-  echo "RELEASE_ID: ${RELEASE_ID}"
+  _result "RELEASE_ID: ${RELEASE_ID}"
 }
 
 _release_check() {
@@ -285,7 +311,7 @@ _release_assets() {
     CONTENT_TYPE_HEADER="Content-Type: ${FILETYPE}"
     CONTENT_LENGTH_HEADER="Content-Length: ${FILESIZE}"
 
-    echo "github releases assets ${RELEASE_ID} ${FILENAME} ${FILETYPE} ${FILESIZE}"
+    _command "github releases assets ${RELEASE_ID} ${FILENAME} ${FILETYPE} ${FILESIZE}"
     URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
     curl \
       -sSL \
@@ -305,7 +331,7 @@ _release() {
 
   _release_id
   if [ ! -z "${RELEASE_ID}" ]; then
-    echo "github releases delete ${RELEASE_ID}"
+    _command "github releases delete ${RELEASE_ID}"
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}"
     curl \
       -sSL \
@@ -315,7 +341,7 @@ _release() {
     sleep 5
   fi
 
-  echo "github releases create ${TAG_NAME} ${DRAFT} ${PRERELEASE}"
+  _command "github releases create ${TAG_NAME} ${DRAFT} ${PRERELEASE}"
   URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
   curl \
     -sSL \
@@ -376,7 +402,7 @@ _dispatch() {
 
   ACCEPT_HEADER="Accept: application/vnd.github.v3+json"
 
-  echo "github dispatches create ${GITOPS_REPO} ${EVENT_TYPE} ${TARGET_ID} ${VERSION}"
+  _command "github dispatches create ${GITOPS_REPO} ${EVENT_TYPE} ${TARGET_ID} ${VERSION}"
   URL="https://api.github.com/repos/${GITOPS_REPO}/dispatches"
   curl \
     -sSL \
@@ -415,21 +441,21 @@ _docker_tag() {
 }
 
 _docker_push() {
-  echo "docker build ${DOCKER_BUILD_ARGS} -t ${IMAGE_URI}:${TAG_NAME} -f ${DOCKERFILE} ${BUILD_PATH}"
+  _command "docker build ${DOCKER_BUILD_ARGS} -t ${IMAGE_URI}:${TAG_NAME} -f ${DOCKERFILE} ${BUILD_PATH}"
   docker build ${DOCKER_BUILD_ARGS} -t ${IMAGE_URI}:${TAG_NAME} -f ${DOCKERFILE} ${BUILD_PATH}
 
   _error_check
 
-  echo "docker push ${IMAGE_URI}:${TAG_NAME}"
+  _command "docker push ${IMAGE_URI}:${TAG_NAME}"
   docker push ${IMAGE_URI}:${TAG_NAME}
 
   _error_check
 
   if [ "${LATEST}" == "true" ]; then
-    echo "docker tag ${IMAGE_URI}:latest"
+    _command "docker tag ${IMAGE_URI}:latest"
     docker tag ${IMAGE_URI}:${TAG_NAME} ${IMAGE_URI}:latest
 
-    echo "docker push ${IMAGE_URI}:latest"
+    _command "docker push ${IMAGE_URI}:latest"
     docker push ${IMAGE_URI}:latest
   fi
 }
@@ -467,14 +493,14 @@ _docker_pre() {
 _docker() {
   _docker_pre
 
-  echo "docker login ${REGISTRY} -u ${USERNAME}"
+  _command "docker login ${REGISTRY} -u ${USERNAME}"
   echo ${PASSWORD} | docker login ${REGISTRY} -u ${USERNAME} --password-stdin
 
   _error_check
 
   _docker_push
 
-  echo "docker logout"
+  _command "docker logout"
   docker logout
 }
 
@@ -520,17 +546,17 @@ text
 EOF
 
   # https://docs.aws.amazon.com/cli/latest/reference/ecr/get-login.html
-  # echo "aws ecr get-login --no-include-email"
+  # _command "aws ecr get-login --no-include-email"
   # aws ecr get-login --no-include-email | sh
 
-  echo "aws ecr get-login-password ${AWS_ACCOUNT_ID} ${AWS_REGION}"
+  _command "aws ecr get-login-password ${AWS_ACCOUNT_ID} ${AWS_REGION}"
   aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/
 
   _error_check
 
   COUNT=$(aws ecr describe-repositories --output json | jq '.repositories[] | .repositoryName' | grep "\"${IMAGE_NAME}\"" | wc -l | xargs)
   if [ "x${COUNT}" == "x0" ]; then
-    echo "aws ecr create-repository ${IMAGE_NAME}"
+    _command "aws ecr create-repository ${IMAGE_NAME}"
     aws ecr create-repository --repository-name ${IMAGE_NAME} --image-tag-mutability ${IMAGE_TAG_MUTABILITY}
   fi
 
@@ -563,7 +589,7 @@ if [ -z "${CMD}" ]; then
   _error
 fi
 
-echo "[${CMD:2}] start..."
+_result "[${CMD:2}] start..."
 
 case "${CMD:2}" in
   version)
